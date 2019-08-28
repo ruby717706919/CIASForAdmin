cc3dbd5503a605554182b3ed888a0303f46ba59c
Icarus <717706919@qq.com>
2019-08-28 17:42:29 +0800

    管理员端的员工状态显示大体完成

--- src/ui/UserUI.java
+++ src/ui/UserUI.java
@@ -6,18 +6,26 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
 import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 
 import util.*;
 import java.awt.GridLayout;
 
+import java.sql.*;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
 public class UserUI {
 
 	private JFrame frame;
 	private TimeSynchro timeSynchro=new TimeSynchro();
+	private SqlConnect sqlConnect=null;
 
 	/**
 	 * Launch the application.
@@ -39,8 +47,8 @@ public class UserUI {
 	 * Create the application.
 	 */
 	public UserUI() {
+		sqlConnect=new SqlConnect();
 		initialize();
-		
 	}
 
 	/**
@@ -50,22 +58,20 @@ public class UserUI {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(new Color(135, 206, 250));
 		frame.getContentPane().setForeground(Color.WHITE);
-		frame.setBounds(100, 100, 1024, 604);
+		frame.setBounds(100, 100, 1000, 551);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
 		frame.setLocationRelativeTo(null);
+		frame.getContentPane().setLayout(null);
+		frame.setResizable(false);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(0, 255, 255));
+		panel.setBounds(10, 21, 209, 188);
+		panel.setBackground(Color.CYAN);
 		panel.setForeground(Color.WHITE);
-		panel.setBounds(30, 20, 214, 256);
 		frame.getContentPane().add(panel);
-		
-		/*JTextArea textArea = new JTextArea();//显示日期
-		textArea.setBounds(30, 46, 140, 24);
-		panel.add(textArea);*/
 		panel.setLayout(null);
 		JLabel label = new JLabel();
+		label.setBackground(Color.WHITE);
 		label.setBounds(30, 55, 140, 21);
 		panel.add(label);
 		
@@ -84,11 +90,30 @@ public class UserUI {
 		panel.add(label_3);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(58, 176, 83, 52);
+		panel_1.setBounds(59, 118, 83, 52);
 		panel.add(panel_1);
 		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(Color.CYAN);
+		panel_3.setBounds(229, 21, 529, 464);
+		frame.getContentPane().add(panel_3);
+		GridBagLayout gbl_panel_3 = new GridBagLayout();
+		gbl_panel_3.columnWidths = new int[]{0};
+		gbl_panel_3.rowHeights = new int[]{0};
+		gbl_panel_3.columnWeights = new double[]{Double.MIN_VALUE};
+		gbl_panel_3.rowWeights = new double[]{Double.MIN_VALUE};
+		panel_3.setLayout(new GridLayout(5, 4, 10, 10));
+		
+		JButton btnNewButton_1 = new JButton("考勤总体日报");
+		btnNewButton_1.setBounds(778, 21, 183, 66);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("考勤总体周报");
+		btnNewButton_2.setBounds(778, 109, 183, 72);
+		frame.getContentPane().add(btnNewButton_2);
+		
 		JPanel panel_2 = new JPanel();
-		panel_2.setBounds(30, 326, 214, 170);
+		panel_2.setBounds(10, 224, 209, 261);
 		frame.getContentPane().add(panel_2);
 		panel_2.setLayout(null);
 		
@@ -116,28 +141,63 @@ public class UserUI {
 		button_2.setBounds(111, 39, 93, 23);
 		panel_2.add(button_2);
 		
-		JPanel panel_3 = new JPanel();//暂定为显示员工的当日打卡情况，暂不需要实现
-		panel_3.setBounds(254, 20, 534, 477);
-		frame.getContentPane().add(panel_3);
-		panel_3.setLayout(new GridLayout(1, 0, 0, 0));
-		
-		JLabel lblNewLabel = new JLabel("什么都队考勤管理系统");//预定使用图片替换，
-		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setBounds(589, 508, 397, 47);
-		frame.getContentPane().add(lblNewLabel);
-		
-		JButton btnNewButton_1 = new JButton("考勤总体日报");//该button实现查看本日员工考勤情况功能的实现
-		btnNewButton_1.setBounds(798, 20, 188, 74);
-		frame.getContentPane().add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("考勤总体周报");//该button实现查看本月员工考勤情况功能的实现，与上button同考虑使用第三方插件，暂无需实现
-		btnNewButton_2.setBounds(800, 118, 186, 71);
-		frame.getContentPane().add(btnNewButton_2);
-		
-		JPanel panel_4 = new JPanel();//备用功能button区，暂时无用，无需处理
-		panel_4.setBounds(798, 199, 188, 297);
+		JPanel panel_4 = new JPanel();
+		panel_4.setBounds(778, 201, 183, 284);
 		frame.getContentPane().add(panel_4);
 		
+		JLabel lblNewLabel = new JLabel("什么都队考勤管理系统");//预定使用图片替换，
+		lblNewLabel.setBounds(841, 495, 120, 15);
+		lblNewLabel.setForeground(Color.WHITE);
+		frame.getContentPane().add(lblNewLabel);
+		
+	
+		ArrayList<JPanel>PanelList=new ArrayList<JPanel>();
+		ArrayList<JLabel>userStateList=new ArrayList<JLabel>();
+		ArrayList<Users>users=new ArrayList<>();
+		users=sqlConnect.getUsers();
+		for (int i = 0; i < 20; i++) {
+			userStateList.add(new JLabel());
+			PanelList.add(new JPanel());
+		}
+		for (int i=0;i<users.size();i++) {
+			userStateList.get(i).setVisible(false);
+			userStateList.get(i).setOpaque(true);
+			PanelList.add(new JPanel());
+			PanelList.get(i).setVisible(false);
+			PanelList.get(i).add(userStateList.get(i));
+			userStateList.get(i).setHorizontalAlignment(SwingConstants.CENTER);
+			userStateList.get(i).setText(String.format("%d%s", users.get(i).getID(), users.get(i).getName()));
+			System.out.println(users.get(i).getNowState());
+			switch (users.get(i).getNowState()){
+				case "0000":
+					PanelList.get(i).setBackground(new Color(18, 253, 27));
+					break;
+				case "1000":
+					PanelList.get(i).setBackground(new Color(9, 253, 250));
+					break;
+				case "0100":
+					PanelList.get(i).setBackground(new Color(25, 53, 253));
+					break;
+				case "0010":
+					PanelList.get(i).setBackground(new Color(253, 30, 33));
+					break;
+				case "0001":
+					PanelList.get(i).setBackground(new Color(253, 237, 33));
+					break;
+				default:
+					PanelList.get(i).setBackground(new Color(255, 255, 255));
+					break;
+			}
+			
+			panel_3.add(PanelList.get(i));
+			PanelList.get(i).setOpaque(true);
+			PanelList.get(i).setVisible(true);
+			userStateList.get(i).setVisible(true);
+		
+			PanelList.get(i).setVisible(true);
+			userStateList.get(i).setVisible(true);
+		}
+	
 		frame.setVisible(true);
 	}
 }
