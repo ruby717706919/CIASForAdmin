923d37e154dd0061218ee48e6ce829d9292baa12
Icarus <717706919@qq.com>
2019-09-03 15:14:11 +0800

    功能修改与完善

---
+++ src/ui/MonthUI.java
@@ -0,0 +1,43 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MonthUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MonthUI window = new MonthUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MonthUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
